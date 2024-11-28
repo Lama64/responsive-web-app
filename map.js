808560bd517cf9b757fd6fc0f205d2b99e09fb0c
Lars Steudle <uojhg@student.kit.edu>
2024-11-28 20:59:41 +0100

    initial styling, markers from file

--- map.js
+++ map.js
@@ -1,24 +1,39 @@
-let map = L.map('map').setView([49.007, 8.404], 14);
+$(document).ready(function() {
+    let map = L.map('map').setView([49.007, 8.404], 14);
+    let layerControl;
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
 
-map.locate({setView: true, maxZoom: 16});
-let marker = L.marker([49.00893729,8.41777117]).addTo(map);
+    map.locate({watch: true, maxZoom: 16});
 
-function onLocationFound(e) {
-    let radius = e.accuracy;
+    function onLocationFound(e) {
+        let radius = e.accuracy;
+        L.marker(e.latlng).addTo(map)
+            .bindPopup("You are within " + radius + " meters from this point");
+        L.circle(e.latlng, radius).addTo(map);
+    }
+    map.on('locationfound', onLocationFound);
 
-    L.marker(e.latlng).addTo(map)
-        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+    function onLocationError(e) {
+        alert(e.message);
+    }
+    map.on('locationerror', onLocationError);
 
-    L.circle(e.latlng, radius).addTo(map);
-}
-map.on('locationfound', onLocationFound);
-
-function onLocationError(e) {
-    alert(e.message);
-}
-map.on('locationerror', onLocationError);
+    function displayStations(stations) {
+        let layerGroups = {};
+        stations.forEach((values, _key, _map) => {
+            if (`Zone ${values['zone']}` in layerGroups) {
+                layerGroups[`Zone ${values['zone']}`]
+                    .addLayer(L.marker([values['lat'], values['lon']]).bindPopup(values['name']));
+            } else {
+                layerGroups[`Zone ${values['zone']}`] 
+                    = L.layerGroup([L.marker([values['lat'], values['lon']]).bindPopup(values['name'])]);
+            }
+        });
+        layerControl = L.control.layers(null, layerGroups).addTo(map);
+    }
+    window.displayStations = displayStations;
+})
---
+++ stops-parser.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+    //Get content of input file
+    $('#stopInput').change((event) => {
+        const file = event.target.files[0];
+        const reader = new FileReader();
+        let text;
+        reader.readAsText(file);
+
+        reader.onload = (e) => {
+            text = e.target.result;
+            try {
+                displayStations(parseStops(text));
+            } catch (error) {
+                alert(error);
+            }
+        }
+        reader.onerror = () => alert('Error loading file');
+    })
+
+    function parseStops(text) {
+        let lines = text.trim().split(/\r?\n/);
+        const indices = lines[0].split(',').map(value => value.replace(/^"|"$/g, ''));
+        let idIndex = findIndex('stop_id', indices, true);
+        let nameIndex = findIndex('stop_name', indices, true);
+        let latIndex = findIndex('stop_lat', indices, true);
+        let lonIndex = findIndex('stop_lon', indices, true);
+        let zoneIndex = findIndex('zone_id', indices, false);
+        let typeIndex = findIndex('location_type', indices, false);
+        let parentIndex = findIndex('parent_station', indices, false);
+
+        lines = lines.slice(1); // first line contains format, no longer needed
+        let previousParentStation = '';
+        let stations = new Map();
+        for (let line of lines) {
+            // " removed after split
+            const values = line.split(',').map(value => value.replace(/^"|"$/g, ''));
+
+            if (previousParentStation === values[parentIndex] && previousParentStation !== '') {
+                continue; // station only needed once (each track listed seperately)
+            }
+
+            if (values[typeIndex]) { 
+                break; // parent stations listed seperately at end of file with location_type 1, not needed again
+            }
+
+            stations.set(values[idIndex],
+                {name: values[nameIndex], lat: values[latIndex], lon: values[lonIndex], zone: values[zoneIndex]});
+            previousParentStation = values[parentIndex];
+        }
+        return stations;
+    }
+
+    function findIndex(indexName, array, errorOnUndefined) {
+        let index = array.findIndex((element) => element == indexName);
+        if (errorOnUndefined && index === undefined) {
+            throw new Error(`${indexName} needs to be present in file!`);
+        }
+        return index;
+    }
+})
